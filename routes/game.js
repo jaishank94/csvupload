88fdb6d094410f37cfe81a3d7962be35bf684641
Jai Shankar Prasad <jaishankarprasad@Aasthas-MacBook-Pro.local>
2023-08-16 11:34:28 +0530

    Added the parameters4

--- routes/game.js
+++ routes/game.js
@@ -13,7 +13,7 @@ const { authUser } = require("../middlwares/auth");
 const router = express.Router();
 
 router.post("/createGame", authUser, createGame);
-router.get("/getAllGames", getAllGames);
+router.get("/getAllGames/:status", getAllGames);
 router.put("/saveGame/:id", authUser, saveGame);
 router.put("/saveGamer/:id", authUser, saveGamer);
 router.delete("/deleteGame/:id", authUser, deleteGame);
