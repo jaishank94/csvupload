bd8692d0ceb178231aa094ba05eff7779a060b28
aviaku <abhishekmishraxyz@gmail.com>
2023-09-12 23:14:13 +0530

    .

--- routes/auction.js
+++ routes/auction.js
@@ -16,13 +16,13 @@ const { authUser } = require("../middlwares/auth");
 const router = express.Router();
 
 router.post("/createAuction", authUser, createAuction);
-router.post("/getAllAuctions", authUser, getAllAuctions);
+router.post("/getAllAuctions", getAllAuctions);
 router.put("/bid", authUser, bid);
 router.put("/saveAuction/:id", authUser, saveAuction);
 router.get("/auctions/by-user/:userId", getAuctionsByUser);
 router.put("/auctions/:auctionId", authUser, updateAuction);
 router.put("/auctions/cancel/:auctionId", authUser, cancelAuction);
-router.get("/searchAuctions", authUser, searchAuctions);
+router.get("/searchAuctions", searchAuctions);
 router.post("/auctions/createRating", authUser, createRating);
 router.delete("/deleteAuction/:id", authUser, deleteAuction);
 
