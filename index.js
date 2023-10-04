0045b84f0bf41f523b08bff9bb4bf0700efc497b
aviaku <abhishekmishraxyz@gmail.com>
2023-10-04 14:21:12 +0530

    .

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ const serverless = require("serverless-http");
 const userRoute = require("./routes/user");
 dotenv.config();
 
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY, {
+  apiVersion: "2022-08-01",
+});
+
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -20,6 +24,34 @@ app.use(
 app.get("/hello", async (req, res) => {
   res.send({ msg: "Hello World" });
 });
+
+app.get("/config", (req, res) => {
+  res.send({
+    publishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+  });
+});
+
+app.post("/create-payment-intent", async (req, res) => {
+  try {
+    const paymentIntent = await stripe.paymentIntents.create({
+      currency: "EUR",
+      amount: 1999,
+      automatic_payment_methods: { enabled: true },
+    });
+
+    // Send publishable key and PaymentIntent details to client
+    res.send({
+      clientSecret: paymentIntent.client_secret,
+    });
+  } catch (e) {
+    return res.status(400).send({
+      error: {
+        message: e.message,
+      },
+    });
+  }
+});
+
 // Start the cron job
 require("./crons/auctionCron.js");
 require("./crons/paymentCron.js");
