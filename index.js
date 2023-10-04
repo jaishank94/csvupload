456ae4f89837eaa80ea54fa109e019d43fb50878
aviaku <abhishekmishraxyz@gmail.com>
2023-10-04 15:01:24 +0530

    .

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ const serverless = require("serverless-http");
 const userRoute = require("./routes/user");
 dotenv.config();
 
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: "2022-08-01",
-});
+// const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY, {
+//   apiVersion: "2022-08-01",
+// });
 
 const app = express();
 app.use(express.json());
@@ -25,32 +25,32 @@ app.get("/hello", async (req, res) => {
   res.send({ msg: "Hello World" });
 });
 
-app.get("/config", (req, res) => {
-  res.send({
-    publishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
-  });
-});
+// app.get("/config", (req, res) => {
+//   res.send({
+//     publishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+//   });
+// });
 
-app.post("/create-payment-intent", async (req, res) => {
-  try {
-    const paymentIntent = await stripe.paymentIntents.create({
-      currency: "EUR",
-      amount: 1999,
-      automatic_payment_methods: { enabled: true },
-    });
+// app.post("/create-payment-intent", async (req, res) => {
+//   try {
+//     const paymentIntent = await stripe.paymentIntents.create({
+//       currency: "EUR",
+//       amount: 1999,
+//       automatic_payment_methods: { enabled: true },
+//     });
 
-    // Send publishable key and PaymentIntent details to client
-    res.send({
-      clientSecret: paymentIntent.client_secret,
-    });
-  } catch (e) {
-    return res.status(400).send({
-      error: {
-        message: e.message,
-      },
-    });
-  }
-});
+//     // Send publishable key and PaymentIntent details to client
+//     res.send({
+//       clientSecret: paymentIntent.client_secret,
+//     });
+//   } catch (e) {
+//     return res.status(400).send({
+//       error: {
+//         message: e.message,
+//       },
+//     });
+//   }
+// });
 
 // Start the cron job
 require("./crons/auctionCron.js");
