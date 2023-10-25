4887a5a5c7a36ab5a63e3039af649b88165de095
Jai Shankar Prasad <jaishankarprasad@Aasthas-MacBook-Pro.local>
2023-10-25 11:05:19 +0530

    Update12

--- routes/data.js
+++ routes/data.js
@@ -4,5 +4,6 @@ const dataController = require("../controllers/data");
 const router = express.Router();
 
 router.get("/data", dataController.getData);
+router.post("/upload-data", dataController.uploadData);
 
 module.exports = router;
